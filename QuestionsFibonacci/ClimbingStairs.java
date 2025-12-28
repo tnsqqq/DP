358c4654cfb61e1d97880a76eb8536541b1b58ac
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-12-28 21:10:22 +0530

    Climbing Stairs Tabulation

--- QuestionsFibonacci/ClimbingStairs.java
+++ QuestionsFibonacci/ClimbingStairs.java
@@ -31,12 +31,35 @@ public class ClimbingStairs {
 
   }
 
+  // Tabulation
+  // 1. Create Table + initialize(base case)
+  // 2. meaning for index
+  // 3. fill (small to large)
+
+  // ways(1) = ways(0) + ways(-1)
+
+  public static int countWaysTabulation(int n) { // O(n)
+    int dp[] = new int[n + 1]; // 0 to n
+    dp[0] = 1;
+
+    // tabulation
+    for (int i = 1; i <= n; i++) {
+      if (i == 1) {
+        dp[i] = dp[i - 1] + 0;
+      } else {
+        dp[i] = dp[i - 1] + dp[i - 2];
+      }
+    }
+    return dp[n];
+  }
+
   public static void main(String[] args) {
     int n = 5; // n=3 -> 3 & n=4 -> 5 => 8
     int ways[] = new int[n + 1]; // 0 0 0 0
     Arrays.fill(ways, -1); // -1 -1 -1 -1
     // System.out.println(countWays(n));
-    System.out.println(countWays(n, ways));
+    // System.out.println(countWays(n, ways));
+    System.out.println(countWaysTabulation(n));
   }
 }
 
